f1bcf20875246066d7c74d5c6aa54f26561cdfc5
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-06-24 19:23:29 +0200

    UV Index

--- components/Home-components/UVIndex.js
+++ components/Home-components/UVIndex.js
@@ -1,76 +1,69 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default class UVIndex extends Component {
   state = {
-      uvLoaded: false,
-      error: null,
-      uvi: null,
+    location: null,
+    geocode: null,
+    errorMessage: "",
   };
 
-  componentDidMount(){
-      navigator.geolocation.getCurrentPosition(
-          position => {
-              this._getUVI(position.coords.latitude, position.coords.longitude);
-          },
-          error => {
-              this.setState({
-                  error
-              });
-          },
-      );
-  }
-  
-  _getUVI = (lat, lon) => {
-      fetch(
-          `http://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`,
-          {
-              headers: {
-                  "x-access-token": "5e344565bfffc560eaea3c26d3e5e3d3"
-              },
-          })
-      .then(response => response.json())
-      .then(json => {
-          this.setState({
-              uvi: json.result.uv,
-              uvLoaded: true,
-          })
+  getGeocodeAsync = async (location) => {
+    let geocode = await Location.reverseGeocodeAsync(location);
+    this.setState({ geocode });
+  };
+
+  componentDidMount = async () => {
+    console.log("Hello");
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied",
       });
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.Highest,
+    });
+    const { latitude, longitude } = location.coords;
+    this.getGeocodeAsync({ latitude, longitude });
+    this.setState({ location: { latitude, longitude } });
   };
 
   render() {
+    const { location, geocode, errorMessage } = this.state;
 
-      const { uvLoaded, error, uvi, } = this.state;
+    console.log(location, geocode, errorMessage);
 
-      console.log(uvi)
-      
-      return (
-          <View style={styles.container}>
-              <StatusBar hidden={true} />
-          <Text>{uvi}</Text>
-          </View>
-      );
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        <Text>{(location, geocode, errorMessage)}</Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: '#fff',
-},
-errorText: {
-  color: 'red',
-  backgroundColor: 'transparent',
-  marginBottom: 40,
-},
-loading: {
-  flex: 1,
-  backgroundColor: '#FDF6AA',
-  justifyContent: 'flex-end',
-  paddingLeft: 25,
-},
-loadingText: {
-  fontSize: 38,
-  marginBottom: 90,
-},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40,
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 90,
+  },
 });
