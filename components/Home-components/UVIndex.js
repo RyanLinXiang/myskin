946bcfa72ed4e2dc7f6062a423fd97d7c7a30216
Ricky Duncan-Williams <ricky.duncanwilliams@gmail.com>
2020-06-24 20:41:03 +0200

    update

--- components/Home-components/UVIndex.js
+++ components/Home-components/UVIndex.js
@@ -75,3 +75,57 @@ const styles = StyleSheet.create({
     marginBottom: 90,
   },
 });
+
+
+
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, View, StatusBar } from 'react-native';
+// export default class UVIndex extends Component {
+//   state = {
+//       uvLoaded: false,
+//       error: null,
+//       uvi: null,
+//   };
+//   componentDidMount(){
+//       navigator.geolocation.getCurrentPosition(
+//           position => {
+//               this._getUVI(position.coords.latitude, position.coords.longitude);
+//           },
+//           error => {
+//               this.setState({
+//                   error
+//               });
+//           },
+//       );
+//   }
+//   _getUVI = (lat, lon) => {
+//       fetch(
+//           `http://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`,
+//           {
+//               headers: {
+//                   "x-access-token": "5e344565bfffc560eaea3c26d3e5e3d3"
+//               },
+//           })
+//       .then(response => response.json())
+//       .then(json => {
+//           this.setState({
+//               uvi: json.result.uv,
+//               uvLoaded: true,
+//           })
+//       });
+//   };
+//   render() {
+//       const { uvLoaded, error, uvi, } = this.state;
+//       return (
+//           <View style={styles.container}>
+//               <StatusBar hidden={true} />
+//               {(uvi)}
+//           </View>
+//       );
+//   }
+// }
+// const styles = StyleSheet.create({
+// container: {
+//   flex: 1,
+//   backgroundColor: '#fff',
+// });
