b5a76d88b3c65dbd4111b888d3fcbb583d82af8a
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-06-24 17:44:26 +0200

    Added Header

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import MySkinPredict from "./MySkinPredict";
 import MySkinTalk from "./MySkinTalk";
 import Home from "./Home";
 import { ModalWithBackdropShowcase as Playground } from "./Playground";
+import {
+  Icon,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+  Avatar,
+} from "@ui-kitten/components";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import * as globalcss from "../styles/globalcss";
 
 const Navigation = (props) => {
   function predict() {
@@ -16,8 +24,28 @@ const Navigation = (props) => {
     return <MySkinTalk {...props} />;
   }
 
-  function home() {
-    return <Home />;
+  function home({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <SafeAreaView
+          style={{
+            backgroundColor: "transparent",
+            position: "absolute",
+            zIndex: "200",
+            width: "100%",
+          }}
+        >
+          <TopNavigation
+            accessoryLeft={() =>
+              toggleAction(navigation.toggleDrawer, navigation.navigate)
+            }
+            title={renderTitle}
+            alignment="center"
+          />
+        </SafeAreaView>
+        <Home />
+      </View>
+    );
   }
 
   function playground() {
@@ -25,23 +53,75 @@ const Navigation = (props) => {
   }
 
   const Drawer = createDrawerNavigator();
+  const MenuIcon = (props) => <Icon {...props} name="menu-outline" />;
+  const HomeIcon = (props) => <Icon {...props} name="home-outline" />;
 
-  function MyDrawer() {
+  const toggleAction = (handlerToggleDrawer, handlerNavigate) => {
     return (
-      <Drawer.Navigator>
-        <Drawer.Screen name="mySkin: Home" component={home} />
-        <Drawer.Screen name="mySkin: Predict" component={predict} />
-        <Drawer.Screen name="mySkin: Talk" component={talk} />
-        <Drawer.Screen name="Playground" component={playground} />
-      </Drawer.Navigator>
+      <React.Fragment>
+        <TopNavigationAction
+          icon={MenuIcon}
+          onPress={() => handlerToggleDrawer()}
+        />
+        <TopNavigationAction
+          icon={HomeIcon}
+          onPress={() => handlerNavigate("mySkin: Home")}
+        />
+      </React.Fragment>
     );
-  }
+  };
+
+  const renderTitle = (props) => {
+    return (
+      <View style={styles.titleContainer}>
+        <Avatar
+          style={styles.avatar}
+          source={require("../assets/logoavatar.png")}
+          size="giant"
+        />
+      </View>
+    );
+  };
 
   return (
-    <NavigationContainer>
-      <MyDrawer />
-    </NavigationContainer>
+    <React.Fragment>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen
+            name="mySkin: Home"
+            component={home}
+            options={{ title: "Home" }}
+          />
+          <Drawer.Screen
+            name="mySkin: Predict"
+            component={predict}
+            options={{ title: "MySkin: Predict" }}
+          />
+          <Drawer.Screen
+            name="mySkin: Talk"
+            component={talk}
+            options={{ title: "MySkin: Talk" }}
+          />
+          <Drawer.Screen
+            name="Playground"
+            component={playground}
+            options={{ title: "Playground" }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: globalcss.container.backgroundColor },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  avatar: {
+    marginHorizontal: 16,
+  },
+});
 
 export default Navigation;
