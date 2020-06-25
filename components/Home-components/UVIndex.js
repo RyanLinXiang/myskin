d3e3754cce5c80e67c49145dc13f3eb51f9f5fa4
Ricky Duncan-Williams <ricky.duncanwilliams@gmail.com>
2020-06-25 08:15:49 +0200

    update UVIndex

--- components/Home-components/UVIndex.js
+++ components/Home-components/UVIndex.js
@@ -7,6 +7,8 @@ export default class UVIndex extends Component {
   state = {
     location: null,
     geocode: null,
+    uvi: null,
+    uvLoaded:false,
     errorMessage: "",
   };
 
@@ -29,22 +31,44 @@ export default class UVIndex extends Component {
     const { latitude, longitude } = location.coords;
     this.getGeocodeAsync({ latitude, longitude });
     this.setState({ location: { latitude, longitude } });
+    this._getUVI(latitude, longitude);
+
+  };
+
+    _getUVI = (latitude, longitude) => {
+      fetch(
+          `http://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`,
+          {
+              headers: {
+                  "content-type": "application/json",
+                  "x-access-token": "5e344565bfffc560eaea3c26d3e5e3d3"
+              },
+          })
+      .then(response => response.json())
+      .then(json => {
+          this.setState({
+              uvi: json.result.uv,
+              uvLoaded: true,
+          })
+      });
   };
 
   render() {
-    const { location, geocode, errorMessage } = this.state;
+    const { location, geocode, uvi, uvLoaded, errorMessage } = this.state;
 
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {location && geocode ? (
+        {location && geocode && uvLoaded ? (
           <Text>
             {"Latitude: " +
               location.latitude +
               ", Longitude: " +
               location.longitude +
               " Geocode: " +
-              geocode[0].city}
+              geocode[0].city +
+              " UV-Index: " + 
+              uvi.toFixed(1) }
           </Text>
         ) : (
           <Text>UV Index</Text>
@@ -78,54 +102,6 @@ const styles = StyleSheet.create({
 
 
 
-// import React, { Component } from 'react';
-// import { StyleSheet, Text, View, StatusBar } from 'react-native';
-// export default class UVIndex extends Component {
-//   state = {
-//       uvLoaded: false,
-//       error: null,
-//       uvi: null,
-//   };
-//   componentDidMount(){
-//       navigator.geolocation.getCurrentPosition(
-//           position => {
-//               this._getUVI(position.coords.latitude, position.coords.longitude);
-//           },
-//           error => {
-//               this.setState({
-//                   error
-//               });
-//           },
-//       );
-//   }
-//   _getUVI = (lat, lon) => {
-//       fetch(
-//           `http://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`,
-//           {
-//               headers: {
-//                   "x-access-token": "5e344565bfffc560eaea3c26d3e5e3d3"
-//               },
-//           })
-//       .then(response => response.json())
-//       .then(json => {
-//           this.setState({
-//               uvi: json.result.uv,
-//               uvLoaded: true,
-//           })
-//       });
-//   };
-//   render() {
-//       const { uvLoaded, error, uvi, } = this.state;
-//       return (
-//           <View style={styles.container}>
-//               <StatusBar hidden={true} />
-//               {(uvi)}
-//           </View>
-//       );
-//   }
-// }
-// const styles = StyleSheet.create({
-// container: {
-//   flex: 1,
-//   backgroundColor: '#fff',
-// });
+
+
+
