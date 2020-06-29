fdc124755a30a322a8daaebd876ce8db9a01b655
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-06-29 13:48:27 +0200

    Package.JSON : Added reminder

--- components/Home-components/articles/ArticleReminder.js
+++ components/Home-components/articles/ArticleReminder.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, View, Platform } from "react-native";
 import { Text, Button, Card, Calendar } from "@ui-kitten/components";
 import * as stylesArticles from "./stylesArticles";
-import CountDown from "../CountDown";
+import React, { useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const ArticleReminder = (props) => {
-  const [date, setDate] = useState(new Date());
-  const [visible, setVisible] = useState(false);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
 
-  const setTime = (nextDate) => {
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  /* const setTime = (nextDate) => {
     console.log("nextDate:" + nextDate.getTime());
     console.log("Date:" + date.getDate());
     console.log("remain:" + daysRemaining);
@@ -20,35 +40,27 @@ const ArticleReminder = (props) => {
 
     let daysRemaining = Math.abs((chosenDay - currentDay) / oneDay);
     daysRemaining = daysRemaining.toString();
-  };
+  }; */
 
   return (
-    <React.Fragment>
-      <CountDown />
-      <Text>253 Tage</Text>
-      <Text>Ihr nächster Termin ist am </Text>
-
-      <Button size="tiny" onPress={() => setVisible(true)}>
-        Show
-      </Button>
-
-      <Card disabled={true}>
-        <Text category="h6">
-          Ausgewähltes Datum: {date.toLocaleDateString()}
-        </Text>
-
-        <Calendar date={date} onSelect={(nextDate) => setDate(nextDate)} />
-
-        <Button
-          size="tiny"
-          onPress={() => setVisible(false)}
-          style={{ alignSelf: "center" }}
-        >
-          Close
-        </Button>
-        <TextInput placeholder="Uhrzeit" />
-      </Card>
-    </React.Fragment>
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
   );
 };
 
